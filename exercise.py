8e4390e1811bf4c16d55a2d5b8039e05dce835a0
Rcha3110 <rcha3110@gmail.com>
2026-06-25 13:12:41 +0530

    comparison operator exercise

---
+++ exercise.py
@@ -0,0 +1,10 @@
+# Comparison Operator Challenge: Create a Python program that asks the user for their age and prints:
+
+# "You are an adult" if the age is greater than or equal to 18.
+# "You are a minor" if the age is less than 18.
+# Use >= and < comparison operators.
+age = int(input("What's your age?? Tell me your age: "))
+if age >= 18:
+    print("You are an adult, oh I never knew that lol so yeah you can access this site")
+else:
+    print("This website is only for adults. You are a minor. Immediately leave the website for your better mental health")
