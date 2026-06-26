8cdd2e103350e7f877e9641a782452d5559ef600
Rcha3110 <rcha3110@gmail.com>
2026-06-26 20:39:36 +0530

    List exercise

---
+++ gogo.py
@@ -0,0 +1,13 @@
+# List Manipulation Exercise:
+
+# Create a list of 5 items (strings or numbers).
+# Add a new item to the end of the list and another at the second position.
+# Remove the third item from the list.
+# Print the list after each operation.
+hyy = [45,76,34,86,45,87,56,7356,57,25,34]
+hyy.append(444)
+hyy.insert(1,16)
+print(hyy)
+hyy.remove(hyy[2])
+print(hyy)
+
