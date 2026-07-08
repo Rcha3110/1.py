8d20798e49d4df5aa6e7b09072dd4f81e13ea1a1
Rcha3110 <rcha3110@gmail.com>
2026-07-08 20:54:24 +0530

    list comp practice

---
+++ microsoft_class1.py
@@ -0,0 +1,11 @@
+# List Manipulation:
+# Create a list of Kannada foods. Use list comprehension to create a new list where each food name is in uppercase.
+ka_foods = ["dosa", "idli", "obattu", "mudde", "neer dose", "mysuru pak", "chicken"]
+new_ka_foods = [i.upper() for i in ka_foods]
+print(new_ka_foods)
+# Sum of Prices:
+
+# Create a dictionary of 5 items with their prices. Write a program that calculates the total price of all items using a for loop.
+# List of Squares:
+
+# Create a list of numbers from 1 to 10. Use list comprehension to generate a list of their squares.
