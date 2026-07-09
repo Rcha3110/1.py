a88e08dcfbe2332e530e3662c108ae86d5d8e4b4
Rcha3110 <rcha3110@gmail.com>
2026-07-09 06:27:31 +0530

    5th

--- microsoft_class1.py
+++ microsoft_class1.py
@@ -28,11 +28,15 @@ for stu in student_data:
     print(f"Student marks:{stu["marks"]}")
     print("-"*20)
 
-# Dictionary Comprehension:
-
-
-# Create a dictionary where the keys are Kannada cities, and the values are their populations. Use dictionary comprehension to filter out cities with populations below 10 lakhs.
 # Nested List Challenge: Write a Python program that takes a list of lists (a 2D list) as input and:
-
 # Prints the entire matrix row by row.
 # Prints the sum of each row in the matrix.
+matrix = []
+tutty = []
+for i in range(3):
+    listy = [int(no) for no in input().split()]
+    matrix.append(listy)
+    total = sum(listy)
+    tutty.append(total)
+print(matrix)
+print(tutty)
